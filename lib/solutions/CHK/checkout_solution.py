74daf8415792e2d8849f2cdcdbe597b489ceab35
sbx_user1051 <sbx_user1051@169.254.157.85>
2019-09-05 11:12:25 +0000

    Thu Sep 05 11:12:25 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,11 +3,11 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    items_list = skus.split()
+    items_list = [x.strip() for x in skus.split(",")]
+    cnt = Counter()
     for i in items_list:
         try:
             int(i)
         except ValueError:
             return -1
-
 
