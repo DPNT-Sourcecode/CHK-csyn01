054a8c08420e5b3e590fa7341955d889ed2924f4
sbx_user1051 <sbx_user1051@169.254.218.45>
2019-09-05 19:03:17 +0000

    Thu Sep 05 19:03:17 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -107,7 +107,8 @@ class SuperMarket:
 
     def special_offers(self):
         if 'B' in self.total_items and self.total_items['E'] >= 2:
-            self.product_b_sum = self.product_b_sum - 30
+            discount = 30 * self.total_items['E'] % 2
+            self.product_b_sum = self.product_b_sum - discount
 
     def checkout(self):
         self.special_offers()
@@ -121,7 +122,8 @@ class SuperMarketException(Exception):
     pass
 
 if __name__ == '__main__':
-    print(checkout("EEEE"))
+    print(checkout("EEEEB"))
+
 
 
 
