a8263e46a59c99075ac4900204f1c8f51cd36089
sbx_user1051 <sbx_user1051@169.254.157.85>
2019-09-05 12:12:06 +0000

    Thu Sep 05 12:12:06 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -11,12 +11,12 @@ def checkout(skus):
     items_counter = [(x, raw_items_list.count(x)) for x in set(raw_items_list)]
     for i in items_counter:
         if i[0] == 'A':
-            if i[1] > 3:
+            if i[1] == 3:
                 total_price += 130
             else:
                 total_price += 50 * i[1]
         elif i[0] == 'B':
-            if i[1] > 2:
+            if i[1] == 2:
                 total_price += 45
             else:
                 total_price += 30 * i[1]
@@ -33,4 +33,5 @@ def checkout(skus):
 if __name__ == '__main__':
     a = checkout("AAAA")
     print(a)
+
 
