b57391329f712bc8704777bb34f403e7dcc5b5a9
sbx_user1051 <sbx_user1051@169.254.157.85>
2019-09-05 11:18:25 +0000

    Thu Sep 05 11:18:25 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -7,11 +7,13 @@ from collections import Counter
 
 def checkout(skus):
     items_list = [x.strip() for x in skus.split(",")]
-    count_dict = (x, items_list.count(x) or x in items_list)
-    for i in items_list:
-        try:
-            int(i)
-        except ValueError:
-            return -1
+    count_dict = [(x, items_list.count(x)) for x in set(items_list)]
+    return count_dict
+
+
+if __name__ == '__main__':
+    a = checkout("A, B, C, D")
+    print(a)
+
 
 
