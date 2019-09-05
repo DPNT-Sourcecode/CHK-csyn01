e6c964f6215ffcdc12f59164776e019e4b87455a
sbx_user1051 <sbx_user1051@169.254.157.85>
2019-09-05 11:15:25 +0000

    Thu Sep 05 11:15:25 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -2,12 +2,16 @@
 
 # noinspection PyUnusedLocal
 # skus = unicode string
+from collections import Counter
+
+
 def checkout(skus):
     items_list = [x.strip() for x in skus.split(",")]
-    cnt = Counter()
+    count_dict = (x, items_list.count(x) or x in items_list)
     for i in items_list:
         try:
             int(i)
         except ValueError:
             return -1
+
 
