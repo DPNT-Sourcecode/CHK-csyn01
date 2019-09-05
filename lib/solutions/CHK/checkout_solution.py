a67fbc76ceb209f594dc1cf22b6a083b0734b642
sbx_user1051 <sbx_user1051@169.254.157.85>
2019-09-05 11:27:25 +0000

    Thu Sep 05 11:27:25 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -6,15 +6,28 @@ from collections import Counter
 
 
 def checkout(skus):
+    total_price = 0
     raw_items_list = [x.strip() for x in skus.split(",")]
-    count_dict = [(x, raw_items_list.count(x)) for x in set(raw_items_list)]
+    items_counter = [(x, raw_items_list.count(x)) for x in set(raw_items_list)]
+    for i in items_counter:
+        if i[0] == 'A' and i[1] == 3:
+            total_price += 130
+        elif i[0] == 'B' and i[1] == 2:
+            total_price += 45
+        elif i[0] == 'C':
+            total_price += 20
+        elif i[0] == 'D':
+            total_price += 15
+        else:
+            return -1
 
-    return count_dict
+    return total_price
 
 
 if __name__ == '__main__':
     a = checkout("A, B, C, D, D")
     print(a)
+
 
 
 
