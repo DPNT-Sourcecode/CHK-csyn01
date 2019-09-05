d48d7092c82519860736cc04fa79ddc01346dc2e
sbx_user1051 <sbx_user1051@169.254.157.85>
2019-09-05 15:20:49 +0000

    Thu Sep 05 15:20:49 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,4 +1,4 @@
-
+import os
 
 # noinspection PyUnusedLocal
 # skus = unicode string
@@ -10,23 +10,26 @@ def checkout(skus):
     raw_items_list = [x for x in skus]
     items_counter = [(x, raw_items_list.count(x)) for x in set(raw_items_list)]
     for i in items_counter:
-        if i[0] == 'A':
-            if i[1] % 3 == 0:
-                total_price += int(130 * (i[1] / 3))
+        try:
+            if i[0] == 'A':
+                if i[1] % 3 == 0:
+                    total_price += int(130 * (i[1] / 3))
+                else:
+                    total_price += 50 * (i[1] % 3) + (130 * int(i[1] / 3))
+            elif i[0] == 'B':
+                if i[1] % 2 == 0:
+                    total_price += int(45 * (i[1] / 2))
+                else:
+                    total_price += 30 * (i[1] % 2) + (45 * int(i[1] / 2))
+            elif i[0] == 'C':
+                total_price += 20 * i[1]
+            elif i[0] == 'D':
+                total_price += 15 * i[1]
+            elif i[0] == 'E':
+                total_price += 40 * i[1]
             else:
-                total_price += 50 * (i[1] % 3) + (130 * int(i[1] / 3))
-        elif i[0] == 'B':
-            if i[1] % 2 == 0:
-                total_price += int(45 * (i[1] / 2))
-            else:
-                total_price += 30 * (i[1] % 2) + (45 * int(i[1] / 2))
-        elif i[0] == 'C':
-            total_price += 20 * i[1]
-        elif i[0] == 'D':
-            total_price += 15 * i[1]
-        elif i[0] == 'E':
-            total_price += 40 * i[1]
-        else:
+                return -1
+        except ValueError:
             return -1
 
     return total_price
@@ -52,23 +55,34 @@ class SuperMarket:
         if product[0] == 'A':
             self.product_a(product[1])
         elif product[0] == 'B':
-
-
-
+            self.product_a(product[1])
+        elif product[0] == 'E':
+            self.product_b(product[1])
+        else:
+            raise SuperMarketException()
 
     def product_a(self, items):
         """
         calculates product according to its pricing rules
         :param items: int
-        :return:
         """
         pass
 
     def product_b(self, items):
         """
-                calculates product according to its pricing rules
-                :param items: int
-                :return:
-                """
+        calculates product according to its pricing rules
+        :param items: int
+        """
         pass
+
+    def product_e(self, items):
+        """
+        calculates product according to its pricing rules
+        :param items: int
+        """
+        pass
+
+
+class SuperMarketException(Exception):
+    pass
 
